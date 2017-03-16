d4518052baf54d4e2e79d2bcc44ad04ba69e3e1e
Lewis King <lewis@blubolt.com>
2017-03-16 10:21:30 +0000

    [PERSONAL] - Work on personal site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,30 +1,14 @@
 $(document).ready(function(){
 	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
 	    e.preventDefault();
-	 
+
 	    var target = this.hash,
 	    $target = $(target);
-	 
+
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
 	    }, 300, 'swing', function () {
 	        window.location.hash = target;
 	    });
-	});
-});
-
-$(function(){
-	
-	$('.menu').click(function(e) {
-		$('html').toggleClass('menu-active');
-		e.preventDefault();
-	});
-	
-	$('.menu-wrapper a').click(function(e) {
-		$('html').removeClass('menu-active');
-	});
-	
-	$('.menu-close').click(function(e) {
-		$('html').removeClass('menu-active');
 	});
 });
