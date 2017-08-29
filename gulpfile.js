4fc751f0c9ee51439d8756f607fe8b47326d058f
Lewis King <lewisking@me.com>
2017-08-29 19:19:28 +0100

    [personal] - Continued work on new site

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var
 	imagemin = require('gulp-imagemin'),
 	imageminJpegRecompress = require('imagemin-jpeg-recompress'),
 	px2rem = require('gulp-px2rem'),
+	uglify = require('gulp-uglify'),
 	sass = require('gulp-sass'),
 	reload = browserSync.reload
 ;
@@ -23,7 +24,7 @@ gulp.task('browser-sync', function() {
 
 // Move and minify HTML
 gulp.task('html', function() {
-    return gulp.src(['src/*.html', 'src/*/*/.html'])
+    return gulp.src(['src/*.html', 'src/projects/*/*.html'])
         .pipe(htmlmin({
             collapseWhitespace: true,
             minifyJS: true
@@ -32,6 +33,33 @@ gulp.task('html', function() {
 });
 
 
-gulp.task('default', ['browser-sync'], function() {
-	console.log('test');
+// Compile sass and minify
+gulp.task('stylesheets', function() {
+    return gulp.src('src/assets/scss/style.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        // .pipe(px2rem())
+        .pipe(gulp.dest('dist/assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+// Compile JS and minify
+gulp.task('javascript', function() {
+    return gulp.src('src/assets/js/*.js')
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'));
+});
+
+// Restart from scratch
+gulp.task('restart', function(cb) {
+    del(['dist'], cb)
+});
+
+gulp.task('default', ['browser-sync', 'html', 'stylesheets', 'javascript'], function() {
 });
