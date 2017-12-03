ed9470efef601b885bfa3ce3ecf943e040b31615
Lewis King <lewisking@me.com>
2017-12-03 09:40:57 +0000

    Updated; fix issue where month was undefined

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,7 +1,14 @@
 // Set month + 1 to text
-
 var currentMonth = new Date();
 var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var monthArraySet = monthArray[currentMonth.getMonth()+1];
+var monthArraySet;
 
+// Set next month to Jan if we're in December.
+if(currentMonth.getMonth() == 11) {
+  monthArraySet = monthArray[currentMonth.getMonth()-11];
+} else {
+  monthArraySet = monthArray[currentMonth.getMonth()+1];
+}
+
+// Write to the DOM.
 document.getElementById('currentMonth').innerHTML = 'in ' + monthArraySet;
